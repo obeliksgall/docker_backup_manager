7b5ad0d2d14fa183f8c2bc5217c87bb800d84ed2
obeliksgall <mosiu24@gmail.com>
2026-08-20 18:21:46 +0200

    Add files via upload
    add github link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -277,6 +277,18 @@ export default function App() {
               </button>
             </div>
 
+			<a
+			  href="https://github.com/obeliksgall/docker_backup_manager"
+			  target="_blank"
+			  rel="noopener noreferrer"
+			  className="p-2 bg-slate-950 border border-slate-800 hover:bg-slate-800 text-slate-400 hover:text-white rounded-xl transition flex items-center justify-center"
+			  title="GitHub Documentation / Readme"
+			>
+			  <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
+				<path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.39-1.305.705-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/>
+			  </svg>
+			</a>
+
             <div className="text-right hidden sm:block border-l border-slate-800 pl-4 select-none">
               <p className="text-[10px] uppercase tracking-wider font-semibold text-slate-500">{t('logged_in_as')}</p>
               <p className="text-sm font-medium text-slate-200">{username}</p>
